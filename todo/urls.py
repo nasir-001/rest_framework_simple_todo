c7da08a7d9d127ff368f89c8ea91acb528f1a4e9
Nasir Lawal <nasirlawal001@gmail.com>
2022-07-05 18:33:57 +0100

    add task create code

--- todo/urls.py
+++ todo/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("", views.apiOverview, name="api-overview"),
     path("task-list/", views.taskList, name="task-list"),
     path("task-detail/<str:pk>", views.taskDetail, name="task-detail"),
+    path("task-create/", views.taskCreate, name="task-create"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -32,3 +32,11 @@ def taskDetail(request, pk):
     task = Task.objects.get(id=pk)
     serializer = TaskSerializer(task, many=False)
     return Response(serializer.data)
+
+
+@api_view(['POST'])
+def taskCreate(request):
+    serializer = TaskSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
